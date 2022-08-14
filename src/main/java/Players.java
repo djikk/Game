fe28acbcc1589818cf1b38e0fee77fa8d3b7e454
Victor <knightivanhoe1194@gmail.com>
2022-08-14 15:23:57 +0300

    Withiout setters

--- src/main/java/Players.java
+++ src/main/java/Players.java
@@ -9,27 +9,11 @@ public class Players {
         this.strength = strength;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getStrength() {
         return strength;
     }
 
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
 }
